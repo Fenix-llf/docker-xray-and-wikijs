d9fab484d0c1f82b0ab74288dd5522e6ef00b086
root <root@fenix123.buzz>
2024-01-09 07:18:54 +0000

    add wiki

---
+++ fenix.sh
@@ -0,0 +1,5 @@
+export XRAY_DOMAIN="fenix123.buzz"
+export XRAY_UUID="6d5e5717-8a43-4a01-a40d-82cf24572c9e"
+export XRAY_WS_PATH="/ws-path"
+export XRAY_EMAIL="leelinfae@163.com"
+./gen-xray-config.sh
